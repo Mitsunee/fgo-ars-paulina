dcc56d296625f7f29d0f88993a54a17d462864d8
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-09-16 14:55:36 +0200

    add: implement filters in ServantList

--- src/views/ServantsList/index.tsx
+++ src/views/ServantsList/index.tsx
@@ -1,24 +1,61 @@
-import { useState } from "preact/hooks";
+import { Searcher, sortKind } from "fast-fuzzy";
+import { useMemo, useState } from "preact/hooks";
 import { useAccount } from "~/client/account";
+import { useServantsData } from "~/client/context";
 import { changeRoute } from "~/client/router";
+import { createFilterCheck, useFilters } from "~/client/servantsFilter";
 import { ButtonRow } from "~/components/ButtonField";
 import { NoAccountError } from "~/components/NoAccountError";
+import { ServantFiltersForm } from "~/components/ServantFiltersForm";
 import { MaterialOverview } from "./MaterialOverview";
 import { ServantCard } from "./ServantCard";
 import styles from "./ServantList.module.css";
 
 export function ServantListView() {
   const user = useAccount();
+  const servantsData = useServantsData();
   const [selected, setSelected] = useState<number | undefined>();
+  const [query, setQuery] = useState("");
+  const [filters, setFilters] = useFilters();
+  const [searcher, userServants] = useMemo(() => {
+    let servantList =
+      user?.servants.map((servant, idx) => ({
+        servant,
+        data: servantsData[servant.id],
+        idx
+      })) || [];
+
+    if (filters.menu) {
+      const checkFilters = createFilterCheck(filters);
+      servantList = servantList.filter(({ data }) => checkFilters(data));
+    }
+
+    return [
+      new Searcher(servantList, {
+        threshold: 0.8,
+        keySelector: candidate => candidate.data.name,
+        sortBy: sortKind.insertOrder
+      }),
+      servantList
+    ] as const;
+  }, [filters, servantsData, user?.servants]);
 
   if (!user) return <NoAccountError />;
+
+  const result = query ? searcher.search(query) : userServants;
 
   return (
     <>
       <section className="section">
         <h1>Servants</h1>
+        <ServantFiltersForm
+          query={query}
+          setQuery={setQuery}
+          filters={filters}
+          setFilters={setFilters}
+        />
         <ul className={styles.list}>
-          {user.servants.map((servant, idx) => (
+          {result.map(({ servant, idx }) => (
             <ServantCard
               key={`${servant.id}-${idx}`}
               servant={servant}
